80d11aada28e2db87c7c84fda867fb227e27c53d
furtwanglerr <140227918+furtwanglerr@users.noreply.github.com>
2024-09-14 21:05:42 +0900

    Create hg.js

---
+++ commands/hg.js
@@ -0,0 +1,24 @@
+const { EmbedBuilder } = require('discord.js');
+const config = require("../config.js");
+
+module.exports = {
+  name: "한강수온",
+  description: "한강수온을 알려드려요!",
+  permissions: "0x0000000000000800",
+  options: [],
+  run: async (client, interaction) => {
+    try {
+      const rrr = Math.floor(Math.random() * (9999 - (-100) + 1)) - 100;
+
+      const embed = new EmbedBuilder()
+        .setColor(config.embedColor)
+        .setTitle(`한강수온을 알려드려요!`) 
+        .setDescription(rrr + '°C')
+      
+
+      return interaction.reply({ embeds: [embed] });
+    } catch (e) {
+      console.error(e);
+    }
+  },
+};
