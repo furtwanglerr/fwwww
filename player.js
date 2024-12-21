ed2605abb845b82d0bd3e55aed049d8306031024
furtwanglerr <140227918+furtwanglerr@users.noreply.github.com>
2024-12-21 10:11:52 +0900

    업데이트 player.js

--- player.js
+++ player.js
@@ -24,8 +24,8 @@ function initializePlayer(client) {
             const guild = client.guilds.cache.get(guildId);
             if (guild) guild.shard.send(payload);
         },
-        defaultSearchPlatform: "ytmsearch",
-        restVersion: "v4"
+        defaultSearchPlatform: "spsearch",
+        restVersion: "v3"
     });
 
     let currentTrackMessageId = null;
