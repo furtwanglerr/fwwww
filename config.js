fe47a737ca0b287e4bfa9fcbbde08d4720bcca59
furtwanglerr <140227918+furtwanglerr@users.noreply.github.com>
2024-12-29 20:36:48 +0900

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ module.exports = {
   commandsDir: './commands',  
   embedColor: "#1db954",
   musicardTheme:"themes16", //Goes from themes1 to themes19
-  activityName: "유튜브",
-  activityType: "WATCHING",  // Available activity types : LISTENING , PLAYING
+  activityName: "테스트",
+  activityType: "PLAYING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://youtu.be/Wcgd1oCbW4g?si=ZR0_R8xT7R9Vt-lv",
   CheckmarkIcon: "https://cdn.discordapp.com/attachments/1230824451990622299/1230836684774576168/7762-verified-blue.gif",
   MusicIcon:"https://media.discordapp.net/attachments/1230824451990622299/1236664581364125787/music-play.gif",
